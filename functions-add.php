40e5f974358a12bea1fc43923a94192112066f17
hirokazunaiki <55181103+hirokazunaiki@users.noreply.github.com>
2021-07-02 00:32:43 +0900

    Update functions-add.php

--- functions-add.php
+++ functions-add.php
@@ -1,6 +1,6 @@
 //Shortcode Swiper Template
 function swiper_template( $params = array() ) {
-    extract( shortcode_atts( array('file' => 'default'), $params ));
+    extract( shortcode_atts( array( 'file' => 'default' ), $params ));
     ob_start();
     include( STYLESHEETPATH . "/$file.php" );
     return ob_get_clean();
