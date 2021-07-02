f257df7b4b73e07726cf4a8c7dc47d8de618b487
hirokazunaiki <55181103+hirokazunaiki@users.noreply.github.com>
2021-07-02 10:56:27 +0900

    Update header-add.php

--- header-add.php
+++ header-add.php
@@ -1,3 +1,3 @@
-<!-- Add code between <head>...</head> -->
+<!-- Add code between to "<head>...</head>" -->
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
