bdd1d7770c5cfcf06b26e5a15b3df2f2acc154bd
hirokazunaiki <55181103+hirokazunaiki@users.noreply.github.com>
2021-07-05 00:32:38 +0900

    Delete header-add.php

--- header-add.php
+++
@@ -1,3 +0,0 @@
-<!-- Add code between to "<head>...</head>" -->
-<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
